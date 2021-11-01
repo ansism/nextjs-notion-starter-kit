f1cb1b3e7d683686cd546a5f95343c6b27985380
ansism <81218050+ansism@users.noreply.github.com>
2021-11-01 22:00:12 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,23 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'b836760999354b48a1cf9443b0f3ecff?v=eecc565fba1a4124984e5d55e204ea13',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Myblog',
+  domain: 'https://lapis-lightyear-0b5.notion.site/b836760999354b48a1cf9443b0f3ecff?v=eecc565fba1a4124984e5d55e204ea13',
+  author: 'Tang',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Myblog',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
